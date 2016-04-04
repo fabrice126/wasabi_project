364f111001c9c42752e249ffcc64ca8e69820760
Fabrice <fjauvat@gmail.com>
2016-04-04 19:58:59 +0200

    no message

--- mongo/request_mongo/album.js
+++ mongo/request_mongo/album.js
@@ -1,17 +1,22 @@
-db.getCollection('artistFull').aggregate([ 
+db.getCollection('artist').aggregate([ 
                 {"$match": {"albums": {$exists : true}}},
                 // De-normalize le tableau pour sépérarer les documents
-                {"$unwind": "$albums"},
+//                 {"$unwind": "$albums"},
+                { 
+                    "$unwind": { path: "$albums", includeArrayIndex: "position" } 
+                 },
                 {"$project" : 
                     {
                         _id:0, 
                         "name":1,
                         "titre":"$albums.titre",
-                        "dateSortie":"$albums.dateSortie",
-                        "urlWikipedia":"$albums.urlWikipedia",
-                        "genre":"$albums.genre",
+                        "dateSortie":"$albums.dateSortie",
+                        "urlWikipedia":"$albums.urlWikipedia",
+                        "genre":"$albums.genre",
                         "length":"$albums.length",
-                        "urlAlbum":"$albums.urlAlbum",
+                        "urlAlbum":"$albums.urlAlbum",
+                        "position":1
+
                     }
                 },
                 { 
--- mongo/request_mongo/song.js
+++ mongo/request_mongo/song.js
@@ -1,8 +1,11 @@
-db.getCollection('artistFull').aggregate([ 
+db.getCollection('artist').aggregate([ 
                 {"$match": {"albums.songs.titre": {$exists : true}}},
                 // De-normalize le tableau pour sépérarer les documents
                 {"$unwind": "$albums"},
-                {"$unwind": "$albums.songs"},
+//                 {"$unwind": "$albums.songs"},
+                { 
+                    "$unwind": { path: "$albums.songs", includeArrayIndex: "position" } 
+                },
                 {"$project" : 
                     {
                         _id:0, 
@@ -10,8 +13,9 @@ db.getCollection('artistFull').aggregate([
                         "albumTitre":"$albums.titre",
                         "titre":"$albums.songs.titre",
                         "urlSong":"$albums.songs.urlSong",
-                        "lyrics":"$albums.songs.lyrics",
-                        "urlWikipedia":"$albums.songs.urlWikipedia"
+                        "lyrics":"$albums.songs.lyrics",
+                        "urlWikipedia":"$albums.songs.urlWikipedia",
+                        "position":1
                     }
                 },
                 { 
