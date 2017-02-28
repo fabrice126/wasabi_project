5833dcecb260263c7899e5f941ac05f7029e088d
fabrice126 <fjauvat@gmail.com>
2017-02-28 21:38:38 +0100

    Mise en place de https et de http 2

--- mongo/request_mongo/sanitizeDBFields.js
+++ mongo/request_mongo/sanitizeDBFields.js
@@ -1,35 +1,31 @@
-db['artist'].find({
-    name: "Metallica"
-}).forEach(function (obj) {
+var artistCollection = "artist", albumCollection = "album", songCollection = "song";
 
-    (function sanitizeProperties(obj, this) {
-
-        for (var property in obj) {
-
-            if (obj.hasOwnProperty(property)) {
-
-                if (property == "merbers" || property == "formerMerbers" || property == "activeYears") {
-
-                    if (typeof obj[property] == "object") {
-
-                        sanitizeProperties(obj[property], this);
-
-                    } else {
-
-                        //On supprime les \n\r une fois supprimer on supprime les espaces de fin
-
-                        obj[property] = obj[property].replace(/^(\s)|(\\n|\\r)/g, '').replace(/(\s+)$/g, '');
-
-                        this.print(obj[property]);
-
-                    }
-
-                }
-
-            }
-
+function sanitizeCollection(collection){
+    var i = 0;
+    db[collection].find({},{rdf:0,wordCount:0}).forEach(function (obj) {
+        if(i%5000 == 0){
+            print(collection+": "+i);
         }
-
-    })(obj)
-    //         db['artist'].update({_id:obj._id},{ $set: obj } );
-});
+        i++;
+        (function sanitizeProperties(obj) {
+            for (var property in obj) {
+                if(property !== "_id"){
+                    //print("\n__"+property+"__");
+                    if (typeof obj[property] == "object") {
+                        sanitizeProperties(obj[property]);
+                    } else {
+                        //On supprime les \n\r et les espaces de fin et de début
+                        if(typeof obj[property] === "string" && obj[property] != ""){
+                            obj[property] = obj[property].trim();
+                            //print("  "+property+"=>"+obj[property]);
+                        }
+                    }
+                }
+            }
+        })(obj)
+        db[collection].update({_id:obj._id},{ $set: obj } );
+    });
+}
+sanitizeCollection(artistCollection);
+sanitizeCollection(albumCollection);
+sanitizeCollection(songCollection);
